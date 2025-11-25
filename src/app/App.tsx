0934c07f5bb22bdf58c257a814489f65e82b9b25
Oksana Pozdniak <pozdnyakoks@mail.ru>
2025-11-25 16:00:56 -0300

    add fsd structure to proj

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,12 @@
 import "./styles/index.scss";
 import { Route, Routes } from "react-router-dom";
 import { Link } from "react-router-dom";
-import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
-import { MainPageAsync } from "./pages/MainPage/MainPage.async";
-import { Suspense, useContext } from "react";
-import { Theme, ThemeContext } from "./theme/ThemeContext";
-import { useTheme } from "./theme/useTheme";
-import { classNames } from "./helpers/classNames/classNames";
+import { Suspense } from "react";
+
+import { useTheme } from "./providers/ThemeProvider";
+import { AboutPage } from "pages/AboutPage";
+import { MainPage } from "pages/MainPage";
+import { classNames } from "shared/lib/classNames/classNames";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -18,8 +18,8 @@ const App = () => {
       <Link to="/about">О сайте</Link>
       <Suspense fallback={<div>Загрузка...</div>}>
         <Routes>
-          <Route path={"about"} element={<AboutPageAsync />} />
-          <Route path={"/"} element={<MainPageAsync />} />
+          <Route path={"about"} element={<AboutPage />} />
+          <Route path={"/"} element={<MainPage />} />
         </Routes>
       </Suspense>
     </div>
