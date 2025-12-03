df1700bd5cf7bf8d4a27426dd2c74eb5d21e8052
Oksana Pozdniak <pozdnyakoks@mail.ru>
2025-12-03 14:54:07 -0300

    add jest, add tests

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,22 +1,22 @@
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
 import { Suspense } from "react";
+import { useTranslation } from "react-i18next";
 import { Routes, Route } from "react-router-dom";
 import { routeConfig } from "shared/config/routeConfig/routeConfig";
 
 export function AppRouter() {
+  const { t } = useTranslation();
   return (
-    <Suspense fallback={<div>Загрузка...</div>}>
+    <Suspense fallback={<div>{t("загрузка")}</div>}>
       <Routes>
         {Object.values(routeConfig).map(({ element, path }) => (
           <Route
             key={path as string}
             path={path}
-            element={(
-              <Suspense fallback={<div>Загрузка...</div>}>
+            element={
+              <Suspense fallback={<div>{t("загрузка")}</div>}>
                 <div className="page-wrapper">{element}</div>
               </Suspense>
-            )}
+            }
           />
         ))}
       </Routes>
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,24 +2,26 @@ import React from "react";
 import { classNames } from "shared/lib/classNames/classNames";
 import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
 import cls from "./Navbar.module.scss";
+import { useTranslation } from "react-i18next";
 
 interface NavbarProps {
   className?: string;
 }
 
 export function Navbar({ className }: NavbarProps) {
+  const { t } = useTranslation();
   return (
     <div className={classNames(cls.navbar, {}, [className])}>
       <div className={cls.links}>
         <AppLink theme={AppLinkTheme.RED} className={cls.mainLink} to="/">
-          Главная
+          {t("Главная")}
         </AppLink>
         <AppLink
           theme={AppLinkTheme.SECONDARY}
           className={cls.mainLink}
           to="/about"
         >
-          О сайте
+          {t("О сайте")}
         </AppLink>
       </div>
     </div>
